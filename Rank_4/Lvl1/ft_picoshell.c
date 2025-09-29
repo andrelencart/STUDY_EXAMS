547f737cc8770f970c904f7c801d0a22b3f4f8fe
andrelencart <andre.lencart@gmail.com>
2025-09-29 19:31:59 +0100

    Study Rank4

---
+++ Rank_4/Lvl1/ft_picoshell.c
@@ -0,0 +1,81 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int picoshell(char **cmds[])
+{
+    int i = 0;
+    int fd[2];
+    int ret = 0;
+    pid_t pid;
+    int status;
+
+    while (cmds[i])
+    {
+        if (cmds[i + 1])
+        {
+            if (pipe(fd) == -1)
+                return 1;
+        }
+        pid = fork();
+        if (pid < 0)
+        {
+			if(fd[0] >= 0)
+        		close(fd[0]);
+			if(fd[1] >= 0)
+            	close(fd[1]);
+            return 1;
+        }
+        if (pid == 0)
+        {
+			if (cmds[i + 1])
+        	{
+				close(fd[0]);
+            	if (dup2(fd[1], 1) == -1)
+            	    exit(1);
+				close(fd[1]);
+			}
+            execvp(cmds[i][0], cmds[i]);
+            exit(1);
+        }
+        else
+        {
+			if(cmds[i + 1])
+			{
+				close(fd[1]);
+            	if (dup2(fd[0], 0) == -1)
+            		    exit(1);
+				close(fd[0]);
+			}
+		}
+		i++;
+    }
+    while (wait(&status) > 0)
+    {
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+            ret = 1;
+        else if (!WIFEXITED(status))
+            ret = 1;
+    }
+
+    return ret;
+}
+#include <stdio.h>
+
+int main(void)
+{
+// Example: ls -l | grep main | wc -l
+// Each command is an array of arguments, ending with NULL
+char *cmd1[] = {"ls", "-l", NULL};
+char *cmd3[] = {"wc", "-l", NULL};
+
+// Array of pointers to each command array, ending with NULL
+char **cmds[] = {cmd1,  cmd3, NULL};
+
+int result = picoshell(cmds);
+if (result == 0)
+	printf("All commands executed successfully.\n");
+else
+	printf("One or more commands failed.\n");
+return result;
+}
